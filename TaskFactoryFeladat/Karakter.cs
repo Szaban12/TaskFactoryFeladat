c7802a78c719dcb0b76edff6162c4b54d52045eb
gal.kristof02@gmail.com <gal.kristof02@gmail.com>
2022-03-01 15:42:06 +0100

    First update

---
+++ TaskFactoryFeladat/Karakter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFactoryFeladat
+{
+    class Karakter
+    {
+        public int AX { get; set; }
+        public int AY { get; set; }
+        public int PX { get; set; }
+        public int PY { get; set; }
+        public int DX { get; set; }
+        public int DY { get; set; }
+        public System.ConsoleColor MyColor { get; set; }
+        public char MyChar { get; set; }
+        public Karakter()
+        {
+            Random r = new Random();
+            PX = r.Next(2,50);
+            PY = r.Next(2,25);
+            DX = r.Next(-1,2);
+            DY = r.Next(-1,2);
+            int vel = r.Next(2);
+            if (vel==0)
+            {
+                MyChar = '@';
+            }
+            else if(vel==1)
+            {
+                MyChar = '&';
+            }
+            vel = r.Next(0, 3);
+            if (vel==0)
+            {
+                MyColor = ConsoleColor.Red;
+            }
+            else if (vel==1)
+            {
+                MyColor = ConsoleColor.Green;
+            }
+            else
+            {
+                MyColor = ConsoleColor.Blue;
+            }
+            
+        }
+        public void Draw()
+        {
+            Console.ForegroundColor = System.ConsoleColor.Black;
+            Console.SetCursorPosition(PX, PY);
+            Console.Write(' ');
+            Console.ForegroundColor = MyColor;
+            
+            Console.SetCursorPosition(AX, AY);
+            Console.Write(MyChar);
+        }
+        public void Move()
+        {
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(500);
+                PX = AX;
+                PY = AY;
+                AX = PX + DX;
+                AY = PY + DY;
+                if (AX < 0)
+                {
+                    AX = 0;
+                }
+                if (AY < 0)
+                {
+                    AY = 0;
+                }
+                Draw();
+            }
+            //System.Threading.Thread.Sleep(100);
+            //PX = AX;
+            //PY = AY;
+            //AX = PX + DX;
+            //AY = PY + DY;
+            //Draw();
+        }
+        public Karakter(int aX, int aY, int pX, int pY, int dX, int dY, ConsoleColor myColor, char myChar)
+        {
+            AX = aX;
+            AY = aY;
+            PX = pX;
+            PY = pY;
+            DX = dX;
+            DY = dY;
+            MyColor = myColor;
+            MyChar = myChar;
+        }
+    }
+}
--- TaskFactoryFeladat/Program.cs
+++ TaskFactoryFeladat/Program.cs
@@ -10,6 +10,18 @@ namespace TaskFactoryFeladat
     {
         static void Main(string[] args)
         {
+            Karakter[] k = new Karakter[20];
+            for (int i = 0; i < k.Length; i++)
+            {
+                k[i] = new Karakter();
+            }
+            Task[] taskArray = new Task[19];
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                taskArray[i] = Task.Factory.StartNew(() => k[i].Move());
+            }
+            System.Threading.Thread.Sleep(15000);
+            Console.ReadKey();
         }
     }
 }
